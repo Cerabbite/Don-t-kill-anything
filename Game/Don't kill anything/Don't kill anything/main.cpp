5ceedcd3dd30c9c3631e5a02c7b1b4ef364c3008
KevinGiesberts <kevin.giesberts@gmail.com>
2022-03-08 19:40:23 +0100

    Following tutorial by Suraj Sharma

--- Game/Don't kill anything/Don't kill anything/main.cpp
+++ Game/Don't kill anything/Don't kill anything/main.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
+
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::Window window(sf::VideoMode(640,480), "Don't Kill Anything", sf::Style::Titlebar | sf::Style::Close);
+    sf::Event ev;
     return 0;
 }
