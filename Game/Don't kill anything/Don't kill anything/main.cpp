e3ded4d0250c70053bde29c77f4dc7f25d8c1b82
KevinGiesberts <kevin.giesberts@gmail.com>
2022-03-08 19:49:55 +0100

    We have a window!!

--- Game/Don't kill anything/Don't kill anything/main.cpp
+++ Game/Don't kill anything/Don't kill anything/main.cpp
@@ -8,7 +8,39 @@
 
 int main()
 {
-    sf::Window window(sf::VideoMode(640,480), "Don't Kill Anything", sf::Style::Titlebar | sf::Style::Close);
+    // Window
+    sf::RenderWindow window(sf::VideoMode(640,480), "Don't Kill Anything", sf::Style::Titlebar | sf::Style::Close);
     sf::Event ev;
+
+    // Game loop
+    while (window.isOpen())
+    {
+        // Event polling
+        while (window.pollEvent(ev))
+        {
+            switch (ev.type)
+            {
+            case sf::Event::Closed:
+                window.close();
+                break;
+            case sf::Event::KeyPressed:
+                if (ev.key.code == sf::Keyboard::Escape)
+                    window.close();
+                break;
+            }
+        }
+
+        // Update
+
+        // Render
+        window.clear(sf::Color::Blue); // Clear frame
+
+        // Draw
+
+
+        window.display(); // Tell SFML that window is done drawing
+    }
+
+    // End game
     return 0;
 }
